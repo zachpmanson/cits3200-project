11034ad4fd7d294a6f6330a21e1106d007e7d39c
Zarhmon <zarhmon.k@gmail.com>
2022-08-14 11:34:14 +0800

    updated ui , fixed text_window bug, need more work

--- chatbot/interface.py
+++ chatbot/interface.py
@@ -2,13 +2,13 @@ from logging import root
 from tkinter import *
 from tkinter import ttk
 
+
 def create_window(getReply):
 
-    # Scroll wheel + Enter Button + Menu Buttons are NON-Functioning, not sure if required.
-    # Using basic responses to test user & bot responses
-    # Can currently type directly into  chat_window - need to fix
+    # Menu buttons not fuunctioning
     # Messages in chat_window need to be from bottom up
     # Bot messages in chat_window need to be alligned right hand side
+    # message_window not returning cursor to top left
 
     # parent window
     main_window = Tk()
@@ -22,11 +22,27 @@ def create_window(getReply):
         reply = getReply(msg)
         print(f"User: {msg}")
         print(f"Bot: {reply}")
+        chat_window.configure(state="normal") # allows inserting into window
         chat_window.insert(END, f"\nUser: {msg}")
+      
+        # chat_window.tag_configure("right", justify='right') # configures window to input text right alligned
+        # chat_window.tag_add("right", 1.0, "end")
+
         chat_window.insert(END, f"\nBot: {reply}")
 
+
+        chat_window.configure(state="disabled") # disables users for inputting directly into window
         message_window.delete("1.0", END) # clears message_window after msg is sent
 
+    # chat window
+    chat_window = Text(main_window, bd=1, bg="Grey", width =50, height = 8, cursor="arrow", wrap=WORD)
+    chat_window.place(x=6, y=6, height= 414, width= 388)
+    chat_window.configure(state="disabled") # disables users for inputting directly into window
+
+    # message window
+    message_window = Text(main_window, bg="grey", width=30, cursor="arrow", wrap=WORD)
+    message_window.bind("<Return>", send)
+    message_window.place(x=6, y=426, height=68, width=388)
 
     # main menu bar - possibly delete
     main_menu = Menu(main_window)
@@ -37,28 +53,13 @@ def create_window(getReply):
     file_menu.add_command(label="Test 2")
     file_menu.add_command(label="Test 3")
 
-    # main menu widgets - possibly delte
+    # main menu widgets - possibly delete
     main_menu.add_cascade(label="Drop Down", menu = file_menu) 
     main_menu.add_command(label="Button 2") 
     main_menu.add_command(label="Button 3") 
     main_window.config(menu=main_menu) 
 
-    # chat window
-    chat_window = Text(main_window, bd=1, bg="Grey", width =50, height = 8, cursor="arrow")
-    chat_window.place(x=6, y=6, height= 414, width= 370)
 
-    # message window
-    message_window = Text(main_window, bg="grey", width=30, cursor="arrow", wrap=WORD)
-    message_window.bind("<Return>", send)
-    message_window.place(x=6, y=426, height=68, width=300)
-
-    # send button (may not need) (non functional - use "Enter" key)
-    enter_button = Button(main_window, text="Enter", bg="grey", activebackground="light grey", width=12, height = 5, command=send)
-    enter_button.place(x=312, y=426, height=68, width=82)
-
-    # scrollbar (may not need) (non functional - use scroll wheel)
-    scrollbar = Scrollbar(main_window, command=chat_window.yview())
-    scrollbar.place(x=375, y=5, height =385)
 
     return main_window
     #main_window.mainloop()
