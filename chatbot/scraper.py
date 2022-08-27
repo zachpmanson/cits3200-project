9c581cf0fd033fa3220a38ec76c24790acf45653
pavo-etc <z1verdeman2@gmail.com>
2022-08-27 17:43:10 +0800

    Add google scholar search

--- chatbot/scraper.py
+++ chatbot/scraper.py
@@ -2,19 +2,34 @@ import bs4
 import requests
 
 def google_search(query):
-    url = 'https://google.com/search?q=' + query
+    url = 'https://google.com/search?q=' + "+".join(query.split())
     req = requests.get( url )
     soup = bs4.BeautifulSoup(req.text, "html.parser")
-    returnVal = ""
+    links = []
     for i, item in enumerate(soup.find_all( 'h3' )):
         if item.parent.get("href") is None: continue
-        print(i, item.getText())
-        print(item.parent.get("href")[7:])
-        returnVal += item.getText() + "\n"
-        returnVal += item.parent.get("href")[7:] + "\n"
-    return returnVal
+        links.append({
+            "title":item.getText(),
+            "link":item.parent.get("href")[7:]
+        })
+    return links
+
+def scholar_search(query):
+    url = 'https://scholar.google.com.au/scholar?q=' + "+".join(query.split())
+    req = requests.get( url )
+    soup = bs4.BeautifulSoup(req.text, "html.parser")
+    links = []
+    for i, item in enumerate(soup.find_all( 'h3' )):
+        if item.a.get("href") is None: continue
+        links.append({
+            "title":item.getText(),
+            "link":item.a.get("href")
+        })
+    return links
 
 if __name__ == "__main__":
-    q = input()
+    q = input("Enter query: ")
     q = q.replace("search ", "")
-    google_search(q)
+    from pprint import pprint
+    pprint(google_search(q))
+    pprint(scholar_search(q))
