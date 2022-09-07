224e5367a389f43c9210425260c1ade3d61d6256
Ebuka Anwasi <ebuka.anwasi@gmail.com>
2022-09-07 12:08:30 +0800

    Jokes function
    tells borderline funny jokes

---
+++ chatbot/jokes.py
@@ -0,0 +1,20 @@
+import pyjokes
+
+#fetches the joke
+joke1 = pyjokes.get_joke(language="en", category='all')
+
+#display the joke
+print(joke1)
+
+#a different category
+
+joke2 = pyjokes.get_joke(language = 'en', category='neutral')
+
+#display the joke
+
+print(joke2)
+
+#fetch a bunch of jokes
+jokes = pyjokes.get_jokes(language='en', category='neutral')
+for i in range(5):
+    print(i+1,".", jokes[i])
