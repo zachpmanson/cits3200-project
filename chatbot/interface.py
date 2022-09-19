b97940eabf2f50f254fbc4b472963da5a63c8a26
Zarhmon <zarhmon.k@gmail.com>
2022-09-19 13:04:52 +0800

    implement auto scroll frame1 + performance testing

--- chatbot/interface.py
+++ chatbot/interface.py
@@ -5,8 +5,10 @@ from itertools import cycle
 from PIL import ImageTk, Image
 import python_avatars as pa
 import time 
-import cairosvg
+# import cairosvg
 from os.path import exists
+
+
 
 # outstanding UI issues
 # chat_window text formating, user and bot responses
@@ -14,7 +16,6 @@ from os.path import exists
 # figuring out hush function requirements
 # custom buttons, gui colour scheme
 # implement avatars to customisation buttons
-# check ios - buttons
 # binding return key to submit button
 # bot binds return key after visiting frame2 to frame1
 
@@ -43,6 +44,7 @@ def create_window(getReply, account):
             #chat_window.tag_add("right", 1.0, "end")
             chat_window.insert(END, f"\nBot: {reply}")
             chat_window.configure(state="disabled") # disables users for inputting directly into window
+            chat_window.see(tk.END)
             
             message_window.delete("1.0", END) # clears message_window after msg is sent
 
@@ -72,13 +74,13 @@ def create_window(getReply, account):
     for frame in (frame1, frame2):
         frame.grid(row=0,column=0,sticky='nsew')
         
-    #================== Frame 1 - Login/Avatar creation code ======================================================#
+    #================== Frame 1 - Avatar creation code ======================================================#
 
-    # Background lbl
+    # Background lbl 
     bg_lbl = tk.Label(frame1,bg='#84CBEE')
     bg_lbl.place(x=6, y=6,height= 488, width= 388)
 
-    # Inserting test avatar image
+    # Inserting avatar image
     def insert_img():
         av_bdr =tk.Frame(frame1, highlightbackground = "black", highlightthickness = 2, bd=0) # creating avatar border
         av_bdr.place(x=124, y=19, height=202, width=152)
@@ -92,17 +94,10 @@ def create_window(getReply, account):
         av_lbl.image =img # saving reference of photo 
         av_lbl.place(x=125, y=20, height=200, width=150) # placing the image label
         av_lbl.configure(image=img) # setting the label to the image
-    insert_img()
-    # Submition button
-    submit_btn = tk.Button(frame1, text='Submit',command=submit_button) 
-    submit_btn.place(x=163, y=438, height=30, width=75)
-    submit_btn.bind('<Return>', show_frame)
-    # submit_btn.focus()
-    
-    # Random avatar button
-    rand_btn = tk.Button(frame1, text="Randomise!", bg='white')    
-    rand_btn.place(x=163, y=300, height=50, width=75)
 
+    insert_img()
+
+    # Creating avatar elemnt types
     list_skin_color = cycle(['TANNED','YELLOW','PALE','LIGHT','BROWN','DARK_BROWN','BLACK'])
     list_eye_type = cycle(['CLOSED', 'CRY', 'DEFAULT', 'EYE_ROLL', 'HAPPY', 'HEART', 'SIDE', 'SQUINT', 'SURPRISED', 'WINK_WACKY', 'WINK', 'X_DIZZY'])
     list_top_type = cycle(['NONE', 'BIG_HAIR', 'BOB', 'BUN', 'CURLY', 'CURVY', 'DREADS', 'FRIDA', 'FRIZZLE', 'FRO_BAND', 'FRO', 'LONG_NOT_TOO_LONG', 'SHAGGY_MULLET', 'SHAGGY', 'SHAVED_SIDES', 'SHORT_CURLY', 'SHORT_DREADS_1', 'SHORT_DREADS_2', 'SHORT_FLAT', 'SHORT_ROUND', 'SHORT_WAVED', 'SIDES', 'STRAIGHT_1', 'STRAIGHT_2', 'STRAIGHT_STRAND'])
@@ -125,6 +120,7 @@ def create_window(getReply, account):
     list_eyebrows_types = ['NONE', 'ANGRY_NATURAL', 'ANGRY', 'DEFAULT_NATURAL', 'DEFAULT', 'FLAT_NATURAL', 'FROWN_NATURAL', 'RAISED_EXCITED_NATURAL', 'RAISED_EXCITED', 'SAD_CONCERNED_NATURAL', 'SAD_CONCERNED', 'UNIBROW_NATURAL', 'UP_DOWN_NATURAL', 'UP_DOWN']
     list_clothing_colors = ['BLACK', 'BLUE_01', 'BLUE_02', 'BLUE_03', 'GRAY_01', 'GRAY_2', 'HEATHER', 'PASTEL_BLUE', 'PASTEL_GREEN', 'PASTEL_ORANGE', 'PASTEL_YELLOW', 'PINK', 'RED', 'WHITE']
 
+    # Setting default avatar
     def change_skin(sk=0, ey=0, ha=0, mo=0, ac=0, sh=0, fh=0, hc=0, eb=0, cc=0):
         skin = 'TANNED'
         eyes = 'DEFAULT'
@@ -193,7 +189,7 @@ def create_window(getReply, account):
             for i in range(len(list_clothe_types)):
                 clothes = next(list_clothe_type)
         
-        #Clothe Colours
+        #Clothe Colors
         if cc == 1:
             clothecolor = next(list_clothing_color)
         if cc == -1:
@@ -233,6 +229,7 @@ def create_window(getReply, account):
             for i in range(len(list_facial_hair_types)):
                 facialhair = next(list_facial_hair_type)
 
+        # Current saved avatar 
         my_avatar = pa.Avatar(
             skin_color=eval('pa.SkinColor.%s' % skin),
             eyes=eval('pa.EyeType.%s' % eyes),
@@ -245,13 +242,15 @@ def create_window(getReply, account):
             eyebrows=eval('pa.EyebrowType.%s' % eyebrows),
             clothing_color=eval('pa.ClothingColor.%s' % clothecolor),
         )
-        my_avatar.render("my_avatar.svg")
-        cairosvg.svg2png(url="my_avatar.svg", write_to="my_avatar.png")
+        # my_avatar.render("my_avatar.svg")
+        # cairosvg.svg2png(url="my_avatar.svg", write_to="my_avatar.png")
     
     change_skin(0,0,0,0,0,0)
 
+#================== Frame 1 - Button creation code ======================================================#
+
     # Hair buttons & label
-    hair_bdr =tk.Frame(frame1, highlightbackground = "black", highlightthickness = 2, bd=0)
+    hair_bdr = tk.Frame(frame1, highlightbackground = "black", highlightthickness = 2, bd=0)
     hair_bdr.place(x=49, y=227, height=42, width=77)
     hair_lbl = tk.Label(frame1, text="Hair", bg='white')
     hair_lbl.place(x=50, y=228, height=40, width=75)
@@ -261,7 +260,7 @@ def create_window(getReply, account):
     hair_left_btn.place(x=24, y=243, height=10, width=20)
 
     # Hair Colour buttons & label
-    hair_col_bdr =tk.Frame(frame1, highlightbackground = "black", highlightthickness = 2, bd=0)
+    hair_col_bdr = tk.Frame(frame1, highlightbackground = "black", highlightthickness = 2, bd=0)
     hair_col_bdr.place(x=49, y=283, height=42, width=77)
     hair_col_lbl = tk.Label(frame1, text="Hair Colour", bg='white')
     hair_col_lbl.place(x=50, y=284, height=40, width=75)
@@ -271,7 +270,7 @@ def create_window(getReply, account):
     hair_col_left_btn.place(x=24, y=299, height=10, width=20)
 
     # Skin buttons & label
-    skin_bdr =tk.Frame(frame1, highlightbackground = "black", highlightthickness = 2, bd=0) # creating background border
+    skin_bdr = tk.Frame(frame1, highlightbackground = "black", highlightthickness = 2, bd=0) # creating background border
     skin_bdr.place(x=49, y=339, height=42, width=77) # placing border
     skin_lbl = tk.Label(frame1, text="Skin", bg='white') # creating skin label
     skin_lbl.place(x=50, y=340, height=40, width=75) # placing skin label
@@ -281,7 +280,7 @@ def create_window(getReply, account):
     skin_left_btn.place(x=24, y=355, height=10, width=20) # placing right button
 
     # Eyes buttons & label
-    eyes_bdr =tk.Frame(frame1, highlightbackground = "black", highlightthickness = 2, bd=0)
+    eyes_bdr = tk.Frame(frame1, highlightbackground = "black", highlightthickness = 2, bd=0)
     eyes_bdr.place(x=49, y=395, height=42, width=77)
     eyes_lbl = tk.Label(frame1, text="Eyes", bg='white')
     eyes_lbl.place(x=50, y=396, height=40, width=75)
@@ -291,7 +290,7 @@ def create_window(getReply, account):
     eyes_left_btn.place(x=24, y=411, height=10, width=20)
 
     # Eyebrows buttons & label
-    brows_bdr =tk.Frame(frame1, highlightbackground = "black", highlightthickness = 2, bd=0)
+    brows_bdr = tk.Frame(frame1, highlightbackground = "black", highlightthickness = 2, bd=0)
     brows_bdr.place(x=49, y=451, height=42, width=77)
     brows_lbl = tk.Label(frame1, text="Eyebrows", bg='white')
     brows_lbl.place(x=50, y=452, height=40, width=75)
@@ -301,7 +300,7 @@ def create_window(getReply, account):
     brows_left_btn.place(x=24, y=467, height=10, width=20)
 
     # Shirt buttons & label
-    shirt_bdr =tk.Frame(frame1, highlightbackground = "black", highlightthickness = 2, bd=0)
+    shirt_bdr = tk.Frame(frame1, highlightbackground = "black", highlightthickness = 2, bd=0)
     shirt_bdr.place(x=274, y=227, height=42, width=77)
     shirt_lbl = tk.Label(frame1, text="Shirt", bg='white')
     shirt_lbl.place(x=275, y=228, height=40, width=75)
@@ -311,7 +310,7 @@ def create_window(getReply, account):
     shirt_left_btn.place(x=357, y=243, height=10, width=20)
     
     # Accessories buttons & label
-    acc_bdr =tk.Frame(frame1, highlightbackground = "black", highlightthickness = 2, bd=0)
+    acc_bdr = tk.Frame(frame1, highlightbackground = "black", highlightthickness = 2, bd=0)
     acc_bdr.place(x=274, y=283, height=42, width=77)
     acc_lbl = tk.Label(frame1, text="Accessories", bg='white')
     acc_lbl.place(x=275, y=284, height=40, width=75)
@@ -321,7 +320,7 @@ def create_window(getReply, account):
     acc_left_btn.place(x=356, y=299, height=10, width=20)
 
     # Mouth buttons & label
-    mouth_bdr =tk.Frame(frame1, highlightbackground = "black", highlightthickness = 2, bd=0)
+    mouth_bdr = tk.Frame(frame1, highlightbackground = "black", highlightthickness = 2, bd=0)
     mouth_bdr.place(x=274, y=339, height=42, width=77)
     mouth_lbl = tk.Label(frame1, text="Mouth", bg='white')
     mouth_lbl.place(x=275, y=340, height=40, width=75)
@@ -331,7 +330,7 @@ def create_window(getReply, account):
     mouth_left_btn.place(x=356, y=355, height=10, width=20)
 
     # Clothing Colours buttons & label
-    nose_bdr =tk.Frame(frame1, highlightbackground = "black", highlightthickness = 2, bd=0)
+    nose_bdr = tk.Frame(frame1, highlightbackground = "black", highlightthickness = 2, bd=0)
     nose_bdr.place(x=274, y=395, height=42, width=77)
     nose_lbl = tk.Label(frame1, text="Clothing Colour", bg='white')
     nose_lbl.place(x=275, y=396, height=40, width=75)
@@ -341,7 +340,7 @@ def create_window(getReply, account):
     nose_left_btn.place(x=356, y=411, height=10, width=20)
 
     # Facial Hair buttons & label
-    mouth_bdr =tk.Frame(frame1, highlightbackground = "black", highlightthickness = 2, bd=0)
+    mouth_bdr = tk.Frame(frame1, highlightbackground = "black", highlightthickness = 2, bd=0)
     mouth_bdr.place(x=274, y=451, height=42, width=77)
     mouth_lbl = tk.Label(frame1, text="Facial Hair", bg='white')
     mouth_lbl.place(x=275, y=452, height=40, width=75)
@@ -350,23 +349,32 @@ def create_window(getReply, account):
     mouth_left_btn = tk.Button(frame1, command= lambda: [change_skin(0, 0, 0, 0, 0, 0, -1, 0, 0, 0), insert_img()])
     mouth_left_btn.place(x=356, y=467, height=10, width=20)
 
+    # Submition button
+    submit_btn = tk.Button(frame1, text='Submit',command=submit_button) 
+    submit_btn.place(x=163, y=438, height=30, width=75)
+    submit_btn.bind('<Return>', show_frame)
     
-
+    # Random avatar button
+    rand_btn = tk.Button(frame1, text="Randomise!", bg='white')    
+    rand_btn.place(x=163, y=300, height=50, width=75)
     
-
     #================== Frame 2 - Chat bot UI code =================================================#
+
     # chat window
     chat_window = Text(frame2, bd=1, bg="#84CBEE", width =50, height = 8, cursor="arrow", wrap=WORD, font= set_font)
     chat_window.place(x=6, y=6, height= 414, width= 388)
     chat_window.configure(state="disabled") # disables users for inputting directly into window
+    
 
+   
     # message window
     message_window = Text(frame2, bg="#84CBEE", width=30, cursor="arrow", wrap=WORD, font= set_font)
     message_window.bind('<Return>', send)
     message_window.place(x=6, y=426, height=68, width=388)
     
 
-    #================== Window button code =========================================================#
+    #================== Root Window buttons code =========================================================#
+
     # main menu bar 
     main_menu = Menu(root_window)
 
@@ -380,6 +388,7 @@ def create_window(getReply, account):
     main_menu.add_cascade(label="Options", menu = file_menu)  # shows drop down button
     root_window.config(menu=main_menu) 
 
+    
     show_frame(frame1)
     
     return root_window
