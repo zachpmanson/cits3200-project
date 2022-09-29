d232d7919708083287ae0f0820c49949c6f49c0d
pavo-etc <z1verdeman2@gmail.com>
2022-09-29 16:54:35 +0800

    Retool server to server chatterbot messages

--- broadcast-server/server.py
+++ broadcast-server/server.py
@@ -1,36 +1,108 @@
-import asyncio
-import websockets
-import ssl
+import socket
+from chatterbot import ChatBot
+from chatterbot.trainers import ListTrainer
+from chatterbot.trainers import ChatterBotCorpusTrainer
+import getopt
+import sys
+import struct
+# ssl_context = ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER)
+# ssl_context.load_cert_chain(
+#     "/etc/letsencrypt/live/cits3200api.zachmanson.com/fullchain.pem",
+#     "/etc/letsencrypt/live/cits3200api.zachmanson.com/privkey.pem"
+# )
 
-host = ""
+
+# Sends packed big endian message 
+def send_msg(sock, msg):
+    if type(msg) == bytes:
+        packed_msg = struct.pack('>I', len(msg)) + msg
+    else:
+        packed_msg = struct.pack('>I', len(msg)) + msg.encode()
+    sock.send(packed_msg)
+
+# Receives a packed bid endian message from a socket
+def recv_msg(sock):
+    packed_msg_len = force_recv_all(sock, 4)
+    if not packed_msg_len:
+        return None
+    msg_len = struct.unpack('>I', packed_msg_len)[0]
+    recved_data = force_recv_all(sock, msg_len)
+    return recved_data
+
+# Ensures that all the bytes we want to read on receival are read
+def force_recv_all(sock, msg_len):
+    all_data = bytearray()
+    while len(all_data) < msg_len:
+        packet = sock.recv(msg_len - len(all_data))
+        if not packet:
+            return None
+        all_data.extend(packet)
+    return all_data
+
+
+# Checks to see if the "-v" flag was used
+optlist, args = getopt.getopt(sys.argv[1:], "v")
+for opt in optlist:
+    if opt[0] == "-v":
+        verbose = True
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+if verbose: print("Opened server")
+if verbose: print("Created socket")
+
+chatbot = ChatBot("Bot1")
+conversation = [
+    "Hello",
+    "Hi there!",
+    "How are you doing?",
+    "I'm doing great.",
+    "That is good to hear",
+    "Thank you.",
+    "You're welcome."
+]
+
+trainer = ListTrainer(chatbot)
+trainer.train(conversation)
+trainer = ChatterBotCorpusTrainer(chatbot)
+trainer.train("chatterbot.corpus.english")
+
+nconnections = 0
+# Default values
 port = 8765
-clients = []
+addr = "0.0.0.0"
+# Bind the socket to a port
+s.bind((addr, port))
+if verbose: print(f"Socket binded to {port}")
 
-ssl_context = ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER)
-ssl_context.load_cert_chain(
-    "/etc/letsencrypt/live/cits3200api.zachmanson.com/fullchain.pem",
-    "/etc/letsencrypt/live/cits3200api.zachmanson.com/privkey.pem"
-)
+# Have the socket listen for a connection
+s.listen(5)
+if verbose: print("Socket is listening")
+s.settimeout(0.1)
 
-async def handler(websocket):
-    clients.append(websocket)
-    print(f"Client connected: {websocket.remote_address}")
-    print(f"{len(clients)} connections")
-    async for message in websocket:
-        # websockets.broadcast(clients, message)
-        for i, client in enumerate(clients):
-            try:
-                await client.send(message)
-            except websockets.exceptions.ConnectionClosed:
-                del clients[i]
-                print(f"Client disconnected: {websocket.remote_address}")
-                print(f"{len(clients)} connections")
+try:
+    while True:
+        try:
+            # Accepts a connection
+            connection, addr = s.accept()
+        except socket.timeout:
+            connection = None
 
-async def main():
-    async with websockets.serve(handler, host, port, ssl=ssl_context) as server:
-        print(f"Websocket open on {host}:{port}")
-        await asyncio.Future()  # run forever
-        
+        # If a connection has been made
+        if connection is not None:
+            nconnections +=1
+            if verbose: print(f"Got connection {nconnections} from {addr}")
+           
+            received_data = recv_msg(connection).decode()
+            if verbose: print("\t<--",received_data)
+            
+            reply = chatbot.get_response(received_data)
+            if verbose: print("\t-->",reply)
+            send_msg(connection, str(reply))
+            
+            connection.close()
 
-if __name__ == "__main__":
-    asyncio.run(main())
+# To close the server ("^C")
+except KeyboardInterrupt:
+    s.close()
+    if verbose: print("\tClosed server")
