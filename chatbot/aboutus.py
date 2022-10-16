d465008b98fd3e5401fcb10c8502bc2b568baa2f
Zarhmon <zarhmon.k@gmail.com>
2022-10-16 14:37:30 +0800

    updated about us link to view google doc link

--- chatbot/aboutus.py
+++ chatbot/aboutus.py
@@ -1,6 +1,6 @@
 import webbrowser
 def launchBrowser():
-    url = "https://github.com/pavo-etc/cits3200-project/blob/main/Official_Chatterbot_Assistant_Documentation.pdf"
+    url = "https://docs.google.com/document/d/1IdKf29CIPlghYLbzzyKt8C37esZ9fpD7/edit?usp=sharing&ouid=114731064788536786465&rtpof=true&sd=true"
     webbrowser.open_new_tab(url)
         
 if __name__ == "__main__":    
