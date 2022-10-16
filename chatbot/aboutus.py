d1ccf07030591b94eb52f2023744633a7a99a21f
Zarhmon <zarhmon.k@gmail.com>
2022-10-16 10:34:54 +0800

    changes aboutUs to user guide linked documentation

--- chatbot/aboutus.py
+++ chatbot/aboutus.py
@@ -1,6 +1,6 @@
 import webbrowser
 def launchBrowser():
-    url = "https://research-repository.uwa.edu.au/en/persons/yulia-shiikha"
+    url = "https://github.com/pavo-etc/cits3200-project/blob/main/Official_Chatterbot_Assistant_Documentation.pdf"
     webbrowser.open_new_tab(url)
         
 if __name__ == "__main__":    
